1391313bab42583d3aa0e74d49dc4fd8548c66a6
Drew Tucker and Victor Felix <victor@>
2018-01-08 16:46:16 -0800

    Add first and last letter capitalization functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-// var userSentence = prompt("Enter a sentence, any sentence");
+var getCipherCap = function(sentence) {
 
-alert("HELP");
+  var firstLetter = sentence[0].toUpperCase();
+  var middle = sentence.slice(1, (sentence.length - 1));
+  var lastLetter = sentence[sentence.length - 1].toUpperCase();
+  var cipher = firstLetter + middle + lastLetter;
+  return cipher;
+}
+
+var giveCipher = function() {
+
+  var sentence = prompt("type something here");
+  alert(getCipherCap(sentence));
+}
+
+giveCipher();
