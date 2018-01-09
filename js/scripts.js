6c38a50941ae6e6a1bc10cc72e9da79f9a119ca9
Drew Tucker and Dillon Titcomb <dillon@>
2018-01-09 10:17:13 -0800

    Add full cipher functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 var getCipherCap = function(sentence) {
 
   var firstLetter = sentence[0].toUpperCase();
@@ -7,10 +8,47 @@ var getCipherCap = function(sentence) {
   return cipher;
 }
 
+var getCipherReverse = function(sentence) {
+
+  var firstLetter = sentence[0].toUpperCase();
+  var middle = sentence.slice(1, (sentence.length - 1));
+  var lastLetter = sentence[sentence.length - 1].toUpperCase();
+  var cipher = lastLetter + middle + firstLetter;
+  return cipher;
+}
+
+var getCipherEnd = function(sentence) {
+
+  var firstLetter = sentence[0].toUpperCase();
+  var middle = sentence;
+  var lastLetter = sentence[sentence.length - 1].toUpperCase();
+  var cipher = middle + firstLetter + lastLetter;
+  return cipher;
+}
+
+var reverse = function(string){
+  return string.split("").reverse().join("");
+}
+
+var getCipherSelect = function(sentence) {
+
+  var firstLetter = sentence[0].toUpperCase();
+  var lastLetter = sentence[sentence.length - 1].toUpperCase();
+  var middle = sentence;
+  var select = Math.floor(sentence.length / 2);
+  var middlePoint = sentence[select];
+  var cipher = middlePoint + middle + firstLetter + lastLetter;
+  var reverseCipher = reverse(cipher);
+  return reverseCipher;
+};
+
 var giveCipher = function() {
 
   var sentence = prompt("type something here");
   alert(getCipherCap(sentence));
+  alert(getCipherReverse(sentence));
+  alert(getCipherEnd(sentence));
+  alert(getCipherSelect(sentence));
 }
 
 giveCipher();
